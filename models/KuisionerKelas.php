e1b6cc9280766c4fb5d64127a7c9b6c317638adf
@ridhobigboy29 <ridhobigboy@gmail.com>
2020-02-28 15:47:47 +0700

    sekian dah

--- models/KuisionerKelas.php
+++ models/KuisionerKelas.php
@@ -65,7 +65,7 @@ class KuisionerKelas extends \yii\db\ActiveRecord
      */
     public function getKelas()
     {
-        return $this->hasOne(Kelas::className(), ['id' => 'kelas_id']);
+        return $this->hasOne(Kelas::className(), ['id.kode.semester' => 'kelas_id']);
     }
 
     /**
@@ -73,7 +73,7 @@ class KuisionerKelas extends \yii\db\ActiveRecord
      */
     public function getJawaban()
     {
-        return $this->hasOne(JawabanKuisioner::className(), ['id' => 'jawaban_id']);
+        return $this->hasOne(JawabanKuisioner::className(), ['id.jawaban' => 'jawaban_id']);
     }
 
     /**
@@ -81,6 +81,45 @@ class KuisionerKelas extends \yii\db\ActiveRecord
      */
     public function getPertanyaan()
     {
-        return $this->hasOne(KuisionerDosen::className(), ['id' => 'pertanyaan_id']);
+        return $this->hasOne(KuisionerDosen::className(), ['id.pertanyaan.aktif.choices' => 'pertanyaan_id']);
     }
 }
+
+$link = mysqli_connect("localhost","root","","DosenAis");
+
+//connection check
+if($link === false) {
+    die("ERROR: could not connect." . mysqli_connect_error());
+}
+
+//attemp select query execution
+$sql = "SELECT * FROM kusioner_kelas WHERE 'pertanyaan' = 'apakah perkuliahaan dimulai & diakhiri tepat waktu' AND 'id' = 1 ";
+$sql = "SELECT * FROM kusioner_kelas WHERE 'pertanyaan' = 'apakah dosen menguasai materi' AND 'id' = 2 ";
+$sql = "SELECT * FROM kusioner_kelas WHERE 'pertanyaan' = 'apakah dosen memberikan feedback' AND 'id' = 3 ";
+
+$sql = "SELECT * FROM kuisioner_kelas WHERE 'jawaban' = 'setuju' ";
+if ($result = mysqli_query($link, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>jawaban</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)) {
+            echo "<tr>";
+                echo"<td>" . $row['id'] . "</td>";
+                echo"<td>" . $row['jawaban'] . "</td>";
+            echo "</tr>";
+        }    
+
+        echo "</table>";
+
+        //close result set
+        mysqli_free_result($result);
+    }else{
+        echo "No record mathcing your query were found";
+    }
+}
+
+mysqli_close($link);
+?>
--- views/site/diagramHasil.php
+++ views/site/diagramHasil.php
@@ -132,7 +132,60 @@ echo Highcharts::widget([
          "plotShadow": false
      },
      "title": {
-         "text": "Ketepatan<br>Waktu",
+         "text": "Pemberian<br>FeedBack",
+         "align": "center",
+         "verticalAlign": "middle",
+         "y": 60
+     },
+     "tooltip": {
+         "pointFormat": "{series.name}: <b>{point.percentage:.1f}%</b>"
+     },
+     "accessibility": {
+         "point": {
+             "valueSuffix": "%"
+         }
+     },
+     "plotOptions": {
+         "pie": {
+             "dataLabels": {
+                 "enabled": true,
+                 "distance": -50,
+                 "style": {
+                     "fontWeight": "bold",
+                     "color": "white"
+                 }
+             },
+             "startAngle": -90,
+             "endAngle": 90,
+             "center": ["50%", "75%"],
+             "size": "110%"
+         }
+     },
+    "series": [{
+         "type": "pie",  
+         "name": "Modul Evaluasi Dosen",
+         "innerSize": "50%",
+         "data": [
+             ["Sangat setuju", 58.9],
+             ["Setuju", 13.29],
+             ["Ragu-Ragu", 13],
+             ["Tidak Setuju", 3.78],
+             ["Sangat Tidak Setuju", 3.42]
+         ]
+     }]
+    }'
+ ]);
+
+
+ echo Highcharts::widget([
+    'options'=>'{
+     "chart": {
+         "plotBackgroundColor": null,
+         "plotBorderWidth": 0,
+         "plotShadow": false
+     },
+     "title": {
+         "text": "Penguasaan<br>Materi",
          "align": "center",
          "verticalAlign": "middle",
          "y": 60
